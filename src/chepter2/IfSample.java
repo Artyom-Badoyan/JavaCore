62c801a737194cbe7bd02ec77c7518306fe1651b
Artyom Badoyan <artyom.styopayevich@gmail.com>
2022-12-14 14:29:33 +0400

    new project

---
+++ src/chepter2/IfSample.java
@@ -0,0 +1,23 @@
+package chepter2;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+        if (x < y) {
+            System.out.println("x меньше y");
+        }
+        x = x * 2;
+        if (x == y) {
+            System.out.println("x теперь равно у");
+        }
+        x = x * 2;
+        if (x > y) {
+            System.out.println("x теперь больше у");
+        }
+        if (x == y) {
+            System.out.println("вы не увидите этого");
+        }
+    }
+}
