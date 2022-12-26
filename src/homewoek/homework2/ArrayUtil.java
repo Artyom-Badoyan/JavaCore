f57a62000dc58bb9c63ef13cebcbf38002ca3ec5
Artyom Badoyan <artyom.styopayevich@gmail.com>
2022-12-26 12:42:40 +0400

    Class changed

--- src/homewoek/homework2/ArrayUtil.java
+++ src/homewoek/homework2/ArrayUtil.java
@@ -5,9 +5,23 @@ import jdk.swing.interop.SwingInterOpUtils;
 public class ArrayUtil {
     public static void main(String[] args) {
 
+        /**
+         Տպեք մասիվի բոլոր էլեմենտները
+         Տպեք մասիվի ամենամեծ թիվը
+         Տպեք մասիվի ամենափոքրը թիվը
+         Տպեք մասիվի բոլոր զույգ էլեմենտները,
+         Տպեք մասիվի բոլոր կենտ էլեմենտները։
+         Տպեք զույգերի քանակը։
+         Տպեք կենտերի քանակը
+         Տպեք մասիվի բոլոր թվերի միջին թվաբանականը (բոլոր էլեմենտների գումարը / էլեմենտների քանակի վրա)
+         Տպեք մասիվի էլեմենտների գումարը։
+         Տպեք մասիվի առաջին էլեմենտը։
+         Տպեք մասիվի վերջին էլեմենտը։
+         Տպեք մասիվի մեջտեղի էլեմենտը։
+         */
+
         int[] array = {5, 10, 8, 15, 4, 6, 7, 20, 25, 14};
 
-        int sum = 0;
         int countEvenNumbers = 0;
         int countOddNumbers = 0;
         int result = 0;
@@ -16,9 +30,14 @@ public class ArrayUtil {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
-
         System.out.println();
 
+        //Տպեք մասիվի առաջին էլեմենտը։
+        //Տպեք մասիվի մեջտեղի էլեմենտը։
+        //Տպեք մասիվի վերջին էլեմենտը։
+        System.out.println("մասիվի առաջին էլեմենտը: " + array[0]);
+        System.out.println("մասիվի մեջտեղի էլեմենտը: " + array[array.length / 2]);
+        System.out.println("մասիվի վերջին էլեմենտը: " + array[array.length - 1]);
 
         //Տպեք մասիվի ամենամեծ թիվը
         int max = array[0];
@@ -28,9 +47,7 @@ public class ArrayUtil {
             }
         }
         System.out.print("մասիվի ամենամեծ թիվը: " + max);
-
         System.out.println();
-
 
         //Տպեք մասիվի ամենափոքրը թիվը
         int min = array[0];
@@ -39,85 +56,40 @@ public class ArrayUtil {
                 min = array[i];
             }
         }
-        System.out.print("մասիվի ամենափոքրը թիվը: " + min);
-
+        System.out.print("մասիվի ամենափոքրը թիվ: " + min);
         System.out.println();
-
 
         //Տպեք մասիվի բոլոր զույգ էլեմենտները
-        for (int i = 0; i < array.length; i++) {
-            if ((array[i] % 2) == 0) {
-                System.out.print(array[i] + " ");
-            }
-        }
-
-        System.out.println();
-
-
-        // Տպեք մասիվի բոլոր կենտ էլեմենտները։
-        for (int i = 0; i < array.length; i++) {
-            if ((array[i] % 2) != 0) {
-                System.out.print(array[i] + " ");
-            }
-        }
-
-        System.out.println();
-
-
-        //Տպեք կենտերի քանակը
-        for (int i = 0; i < array.length; i++) {
-            if ((array[i] % 2) != 0) {
-                countOddNumbers++;
-            }
-        }
-        System.out.print("կենտ էլեմենտների քանակը: " + countOddNumbers);
-
-        System.out.println();
-
-
         //Տպեք զույգերի քանակը։
         for (int i = 0; i < array.length; i++) {
             if ((array[i] % 2) == 0) {
+                System.out.print(array[i] + " ");
                 countEvenNumbers++;
             }
         }
-        System.out.print("զույգ էլեմենտների քանակը: " + countEvenNumbers);
-
         System.out.println();
+        System.out.println("զույգ էլեմենտների քանակը: " + countEvenNumbers);
 
 
+        // Տպեք մասիվի բոլոր կենտ էլեմենտները։
+        // Տպեք կենտերի քանակը
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] % 2) != 0) {
+                System.out.print(array[i] + " ");
+                countOddNumbers++;
+            }
+        }
+        System.out.println();
+        System.out.println("կենտ էլեմենտների քանակը: " + countOddNumbers);
+
+
+        //Տպեք մասիվի էլեմենտների գումարը։
         //Տպեք մասիվի բոլոր թվերի միջին թվաբանականը (բոլոր էլեմենտների գումարը / էլեմենտների քանակի վրա)
         for (int i = 0; i < array.length; i++) {
             result += array[i];
         }
+        System.out.println("Տպեք մասիվի էլեմենտների գումարը: " + result);
         System.out.print("մասիվի բոլոր թվերի միջին թվաբանականը: " + result / array.length);
-
-        System.out.println();
-
-
-        //Տպեք մասիվի էլեմենտների գումարը։
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        System.out.print("մասիվի էլեմենտների գումարը: " + sum);
-
-        System.out.println();
-
-
-        //Տպեք մասիվի առաջին էլեմենտը։
-        System.out.print("մասիվի առաջին էլեմենտը: " + array[0]);
-
-        System.out.println();
-
-
-        //Տպեք մասիվի վերջին էլեմենտը։
-        System.out.print("մասիվի վերջին էլեմենտը: " + array[array.length - 1]);
-
-        System.out.println();
-
-
-        //Տպեք մասիվի մեջտեղի էլեմենտը։
-        System.out.print("մասիվի մեջտեղի էլեմենտը: " + array[array.length / 2]);
 
     }
 }
