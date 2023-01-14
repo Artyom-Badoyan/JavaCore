2e17d0034fa49b62e97e3ac346a90a8cfdbb7143
Artyom Badoyan <artyom.styopayevich@gmail.com>
2023-01-14 16:47:49 +0400

    create new class Stack

---
+++ src/chepter6/Stack.java
@@ -0,0 +1,28 @@
+package chepter6;
+
+public class Stack {
+
+    int array[] = new int[10];
+    int tos;
+
+    Stack() {
+        tos = -1;
+    }
+
+    void push(int item) {
+        if (tos == 9) {
+            System.out.println("Cтeк заполнен.");
+        } else {
+            array[++tos] = item;
+        }
+    }
+
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Cтeк не загружен.");
+            return 0;
+        } else {
+            return array[tos--];
+        }
+    }
+}
