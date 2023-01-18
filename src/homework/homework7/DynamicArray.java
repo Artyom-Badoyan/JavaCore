c37fcde24406bc44f309c9f97320472b97f8f43b
Artyom Badoyan <artyom.styopayevich@gmail.com>
2023-01-18 21:25:21 +0400

    create new package homework7

---
+++ src/homework/homework7/DynamicArray.java
@@ -0,0 +1,48 @@
+package homework.homework7;
+
+public class DynamicArray {
+
+    //սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
+    private int[] array = new int[10];
+    //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
+    private int size = 0;
+
+    //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
+    //և ավելացնենք
+    public void add(int value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    //1․ ստեղծել հին մասիվից 10 էլեմենտ ավելի մեծ մասիվ
+    //2․ քցել հին մասիվի էլեմենտները նորի մեջ
+    //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
+    private void extend() {
+        int[] tmp = new int[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            tmp[i] = array[i];
+        }
+        array = tmp;
+    }
+
+    //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
+    // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
+    public int getByIndex(int index) {
+        for (int i = 0; i < size; i++) {
+            if (index == array[i]) {
+                return array[i];
+            }
+        }
+        return -1;
+    }
+
+    //տպել մասիվի ավելացված էլեմենտները
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+}
+
---
+++ src/homework/homework7/DynamicArrayDemo.java
@@ -0,0 +1,24 @@
+package homework.homework7;
+
+public class DynamicArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.add(1);
+        dynamicArray.add(2);
+        dynamicArray.add(3);
+        dynamicArray.add(4);
+        dynamicArray.add(5);
+        dynamicArray.add(6);
+        dynamicArray.add(7);
+        dynamicArray.add(8);
+        dynamicArray.add(9);
+        dynamicArray.add(10);
+        dynamicArray.add(11);
+        dynamicArray.add(12);
+        dynamicArray.add(13);
+        dynamicArray.add(14);
+        dynamicArray.add(15);
+        dynamicArray.print();
+        System.out.println(dynamicArray.getByIndex(15));
+    }
+}
