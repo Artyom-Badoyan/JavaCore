0d3a716afffe49176af9557a2c22c296826a4ed3
Artyom Badoyan <artyom.styopayevich@gmail.com>
2023-02-26 02:25:08 +0400

    delete comments

--- src/homework/homework10/medicalCenter/model/Doctor.java
+++ src/homework/homework10/medicalCenter/model/Doctor.java
@@ -1,28 +1,5 @@
 package homework.homework10.medicalCenter.model;
 
-
-/*
-Բարի երեկո։
-Ուղարկում եմ գործնական տնայինը, որը պետք է ավարտել մինչև շաբաթ օրվա դասը։
-Սարքում եք medicalCenter փեքիջը, որի մեջ գրելու ենք ծրագիր, որը նախատեսված է  բժշկական հաստատության համար,
-հնարավոր է ունենալ բժիշկներ, և հիվանդներ, ովքեր կարող են հերթագրվել բժիշկների մոտ։
-Ունենալու ենք հետևյալ օբյեկտները։
-Doctor(id, name, surname, email, phoneNumber, profession)
-Patient(id(այդի քարդի համարը), name, surname, phone, doctor(թե որ դոկտորի մոտ է գրանցվել), register date time)
-Քանի որ երկու տիպերը իրար նման են, կարող եք ժառանգականություն օգտագործել, մի հատ Person կլասս սարքեք,
-ընդհանուր դաշտերը իրա մեջ գրեք, մնացածը ամենքը իրա կլասսում։
-Ունենալու ենք հետևյալ հրամանները
-0 - exit
-1 - add doctor
-2 - search doctor by profession
-3 - delete doctor by id
-4 -  change doctor data by id(բացի իդ-ից ամեն ինչ կարողանալու ենք փոխել)
-5 - add patient(ընտրելու եք բժշկին, բայց ստուգեք որ էդ ժամը զբաղված չլինի)
-6 - print all  patients by doctor
-7 - print todays patients
-Ամսաթվերի հետ աշխատանքը կարող եք անել Date-կլասսի մեթոդներով։ Հարցերի դեպքում գրեք չաթի մեջ։
-Հրամանները պտի պահեք Interface-ով։
-Բոլոր հնարավորությունները առանձին սիրուն մեթոդներով արեք, DateUtil-ը նույնպես ինչպես անցել ենք։*/
 public class Doctor extends Person {
 
     private String email;
